760d494b81d80847097e3704ac569adae1e44fad
Jorge Luis <jorgequiguango@outlook.com>
2025-04-15 22:01:44 -0500

    [IMP] Conection to EchoPrint

---
+++ pos_my_ticket/static/src/overrides/components/base_printer.js
@@ -0,0 +1,16 @@
+// import { patch } from '@web/core/utils/patch'
+// import { BasePrinter } from '@point_of_sale/app/printer/base_printer'
+
+// patch(BasePrinter.prototype, {
+//     async printReceipt(receipt) {
+        
+//         console.log('inherit printReceipt() :)')
+//         console.log('receipt', receipt)
+        
+//         // When is error
+//         // return this.getResultsError(null)
+        
+        
+//         return { successful: true }
+//     }
+// })
